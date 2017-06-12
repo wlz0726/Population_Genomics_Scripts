afb1e888dd2365f04c05666241d809e5b56e7b1d
wlz0726 <wanglizhong.lzu@gmail.com>
2017-06-12 17:29:35 +0800

    add msmc

--- 03.treemix/01.tped.to.treemixINPUT.sh
+++ 03.treemix/01.tped.to.treemixINPUT.sh
@@ -1,3 +1,9 @@
 # need input.tped, input.tfam, poporder
 # poporder file with one col (population name per line) or two cols (popname color)
 perl get_treemix_input_from_tped.pl input.tfam input.tped
+
+# OR
+
+# plink1.07 --noweb --tfile input --freq --missing --within treemix.data.clust
+# gzip plink.frq
+# plink2treemix.py plink.frq.gz treemix.frq.gz
